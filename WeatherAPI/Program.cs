b84b7c41da318258f22a6c2d114c8c746fa642e6
SUPERPUPER11 <bob485582@gmail.com>
2023-02-08 20:52:58 +0300

    Добавьте файлы проекта.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Web;
+using WeatherAPI;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.Write("Введите название города: ");
+var apiKey = "de652bea5fc39e262e384811c44b47a7";
+var city = Console.ReadLine();
+var client = new HttpClient();
+
+var response = await client.GetAsync(
+    $"https://api.openweathermap.org/data/2.5/weather?q={HttpUtility.UrlEncode(city)}&appid={apiKey}&units=metric&lang=ru");
+
+var responseDays = await client.GetAsync(
+    $"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apiKey}&units=metric&lang=ru");
+
+if (response.IsSuccessStatusCode)
+{
+    var result = await response.Content.ReadAsStringAsync();
+    var model = JsonConvert.DeserializeObject<WeatherModel>(result);
+    Console.WriteLine($"Погода на сегодня для города {model.name}:");
+    Console.WriteLine($"Текущая температура: {model.Main.Temp}°, {model.Weather[0].Description}");
+    Console.WriteLine($"Ощущается как: {model.Main.FeelsLike}°");
+    Console.WriteLine($"Скорость ветра: {model.Wind.Speed} м/с, {Cardinaldirections(model.Wind.Deg)}.");
+    Console.WriteLine($"Влажность: {model.Main.Humidity} %");
+    Console.WriteLine($"Давление: {model.Main.Pressure} мм рт. ст.");
+}
+else
+{
+    Console.WriteLine("(");
+}
+
+if (responseDays.IsSuccessStatusCode)
+{
+    var resultDays = await responseDays.Content.ReadAsStringAsync();
+    var modelDays = JsonConvert.DeserializeObject<WeatherDays>(resultDays);
+    Console.WriteLine("---------------------------------------------------------------------------------------");
+    Console.WriteLine($"Прогноз погоды с {DateTime.Now.ToString("d")} до {DateTime.Now.AddDays(3).ToString("d")} включительно для города {modelDays.City.Name}:");
+    Console.WriteLine("--------------------------------------------------------------------------------------");
+    Console.WriteLine($"{"Дата и день недели",10} | {"Мин. и макс. температура",35} | {"Описание",25} |");
+    Console.WriteLine($"{WetherStrToDateToStr(modelDays.List[0].DtTxt),18} | мин.тем. {modelDays.List[0].Main.TempMin,5}°  макс. темп. {modelDays.List[0].Main.TempMax,5}° | {modelDays.List[0].Weather[0].Description,25} |");
+    Console.WriteLine($"{WetherStrToDateToStr(modelDays.List[8].DtTxt),18} | мин.тем. {modelDays.List[8].Main.TempMin,5}°  макс. темп. {modelDays.List[8].Main.TempMax,5}° | {modelDays.List[8].Weather[0].Description,25} |");
+    Console.WriteLine($"{WetherStrToDateToStr(modelDays.List[16].DtTxt),18} | мин.тем. {modelDays.List[16].Main.TempMin,5}°  макс. темп. {modelDays.List[16].Main.TempMax,5}° | {modelDays.List[16].Weather[0].Description,25} |"); 
+    Console.WriteLine($"{WetherStrToDateToStr(modelDays.List[24].DtTxt),18} | мин.тем. {modelDays.List[18].Main.TempMin,5}°  макс. темп. {modelDays.List[24].Main.TempMax,5}° | {modelDays.List[24].Weather[0].Description,25} |");
+    Console.WriteLine("--------------------------------------------------------------------------------------");
+    Console.ReadKey();
+}
+
+string Cardinaldirections(int wet) =>
+    wet switch
+    {
+        >= 0 and < 23 or >= 338 and <= 360 => "C",
+        >= 23 and < 68 => "СВ",
+        >= 68 and < 113 => "В",
+        >= 113 and < 158 => "ЮВ",
+        >= 158 and < 203 => "Ю",
+        >= 203 and < 248 => "ЮЗ",
+        >= 248 and < 292 => "З",
+        >= 292 and < 338 => "CЗ",
+        _ => "",
+    };
+string WetherStrToDateToStr(string str1)
+{
+    DateTime Date1 = DateTime.Parse(str1);
+    string strDay = Date1.ToString("dddd");
+    string strDay1 = Date1.ToString("d");
+    return strDay1 + " " + strDay;
+}
---
+++ WeatherAPI/WeatherDays.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI
+{
+    public class WeatherDays
+    {
+        public string Cod { get; set; }
+        public int Message { get; set; }
+        public int Cnt { get; set; }
+        public List[] List { get; set; }
+        public City City { get; set; }
+    }
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+
+        [JsonProperty("timezone")]
+        public int TimeZone { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class List
+    {
+        public int Dt { get; set; }
+        public Root Main { get; set; }
+        public Weather[] Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public int Visibility { get; set; }
+        public float Pop { get; set; }
+        public System Sys { get; set; }
+
+        [JsonProperty("dt_txt")]
+        public string DtTxt { get; set; }
+    }
+
+    public class Root
+    {
+        public float Temp { get; set; }
+        [JsonProperty("feels_like")]
+        public float FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public float TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public float TempMax { get; set; }
+        public int Pressure { get; set; }
+
+        [JsonProperty("sea_level")]
+        public int SeaLevel { get; set; }
+
+        [JsonProperty("grnd_level")]
+        public int GrndLevel { get; set; }
+        public int Humidity { get; set; }
+
+        [JsonProperty("temp_kf")]
+        public float TempKf { get; set; }
+    }
+
+
+    public class System
+    {
+        public string Pod { get; set; }
+    }
+}
